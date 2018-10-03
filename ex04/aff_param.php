523dc12ea829f76a8812a8ca51c3307c356ccfe5
moeketsane SEFAKO <msefako@c3r8s5.wethinkcode.co.za>
2018-10-03 03:03:06 +0200

    modified file structure

--- ex04/aff_param.php
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/php
-<?php
-
-array_shift($argv);
-echo implode("\n", $argv)."\n";
-
-?>
--- ex05/epur_str.php
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/php
-<?php
-
-$str = array_values(array_filter(explode(" ", $argv[1])));
-echo implode(" ", $str)."\n";
-
-?>
